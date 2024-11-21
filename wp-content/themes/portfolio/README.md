bfde92452908ac9cc98ebac4001b09bbd25374b5
Sonie <garachu.muthoni@gmail.com>
2024-11-21 12:59:19 +0300

    first commit

---
+++ wp-content/themes/portfolio/README.md
@@ -0,0 +1,1 @@
+My first wordpress project building rebuilding my portfolio
---
+++ wp-content/themes/portfolio/index.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The main template file
+ * 
+ * This file is required in every theme. It serves as the fallbak template 
+ * when no other templates are available
+ * 
+ * @package PortfolioTheme
+ */
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My WordPress Theme</title>
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+</head>
+<body>
+    Hello World
+</body>
+</html>
